92b2838b200c2f1910c504729330a25f97c10223
Mohammed Hattab <mohammedhattab97@gmail.com>
2024-02-12 20:13:47 -0500

    changed scss

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@
 
 export const environment = {
   production: false,
-  rooturl : 'http://127.0.0.1:5000',
-  // rooturl : 'https://valentine-flask-app-surizikxpq-uc.a.run.app',
+  // rooturl : 'http://127.0.0.1:5000',
+  rooturl : 'https://valentine-flask-app-surizikxpq-uc.a.run.app',
 
 
 };
